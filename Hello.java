7487f97028daea84fef681aba1c7febf423b62fd
Dheeraj <dj171290@gmail.com>
2021-01-03 18:50:17 +0530

    replaced Dhiraj with world

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 	public static void main(String[] args)
 	{
 		System.out.println("helloooo");
-		System.out.println("helloooo Dhiraj");
+		System.out.println("helloooo world");
 	}
 }
